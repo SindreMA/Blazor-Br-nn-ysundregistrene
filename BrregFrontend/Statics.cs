86389c1f5606ed84dbfaedaf717a4fc5d2187188
SindreMA <SindreMA@gmail.com>
2023-11-27 20:34:40 +0100

    Swapped to public api url

--- BrregFrontend/Statics.cs
+++ BrregFrontend/Statics.cs
@@ -2,7 +2,7 @@
 {
     public class Statics
     {
-        //public static string apiUrl = "https://brreg.sindrema.com/api"; // prod server
-        public static string apiUrl = "http://localhost:5050/api"; // dev server
+        public static string apiUrl = "https://brreg.sindrema.com/api"; // prod server
+        //public static string apiUrl = "http://localhost:5050/api"; // dev server
     }
 }
